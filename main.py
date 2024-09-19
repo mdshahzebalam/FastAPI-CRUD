0431cdaf4b577bae535a557c55296ebbd7afd1e2
mdshahzebalam <sus841226@gmail.com>
2024-09-19 15:00:50 +0530

    update

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
 
 app = FastAPI()
 
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
 
 @app.get("/")
 async def root():
