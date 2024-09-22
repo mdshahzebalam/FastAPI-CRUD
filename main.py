0941274b81e90e3a14cdd54ad0f05513ca44d9ec
mdshahzebalam <sus841226@gmail.com>
2024-09-22 16:31:57 +0530

    #required

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def create_item(item: Item | None = None):
     return item
 
 @app.get("/items/")
-async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+async def read_items(q: Annotated[str | None, Query(min_length=3)] = ...):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
