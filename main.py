6b70e88d9642b68a009a2cdaa7937599ae46e735
mdshahzebalam <sus841226@gmail.com>
2024-09-28 16:28:59 +0530

    #Test

--- main.py
+++ main.py
@@ -15,11 +15,16 @@ app = FastAPI()
 
 @app.post("/items/")
 async def create_item(item: Item | None = None):
+
     return item
 
 @app.get("/items/")
 async def read_items(q: Annotated[str | None, Query(min_length=3)] = ...):
+
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+
     if q:
+
         results.update({"q": q})
+        
     return results
