b00ea34daf7bf1e07906a8bbe5854a095f82737b
mdshahzebalam <sus841226@gmail.com>
2024-10-04 20:51:28 +0530

    #Update

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ from fastapi import FastAPI, Query
 from typing import Annotated
 from pydantic import BaseModel
 
-
 class Item(BaseModel):
     name: str
     description: str | None = None
     price: float
     tax: float | None = None
 
-
 app = FastAPI()
-
 
 @app.post("/items/")
 async def create_item(item: Item | None = None):
